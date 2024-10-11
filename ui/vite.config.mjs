b4dc9bfe8a4eb0e4ee9906ffd0c3b139afeeb0d9
Robert Paul <rob@boxbob.io>
2024-10-11 12:04:13 -0500

    added tailwindcss

--- ui/vite.config.mjs
+++ ui/vite.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
+import path from "path";
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr"
 
 export default defineConfig({
   plugins: [react(), svgr()],
   base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 5173
   }
